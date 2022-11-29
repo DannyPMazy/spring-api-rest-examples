6666482ed5f3a3fa5e3dd8ddb7fda54841fb8a5e
Manzoeto <daniele.p.manzoeto@accenture.com>
2022-11-29 15:10:23 +0100

    Updating Groovies Library 15:10

--- library-operations-loan-example/one-library-operations-example/library-first-application-build.groovy
+++ library-operations-loan-example/one-library-operations-example/library-first-application-build.groovy
@@ -6,28 +6,28 @@ pipeline {
     }
 
     stages {
-        stage ('Cloning Repository') {
-            steps {
-                echo "Cloning Repository"
-                sh "git clone https://github.com/DannyPMazy/library-example-rest.git"
-            }
-        }
         stage ("Building Application") {
             steps {
                 echo "Using Maven Install"
-                sh "mvn install"
+                dir("library-operations-loan-example/one-library-operations-example") {
+                    sh "mvn install"    
+                }
             }
         }
         stage ("Create Docker Image") {
             steps {
                 echo "Creating a Docker Image"
-                sh "docker build -t libraryoperations ."
+                dir("library-operations-loan-example/one-library-operations-example") {
+                    sh "docker build -t libraryoperations ."    
+                }
             }
         }
         stage ("Create Docker Container") {
             steps {
                 echo "Creating a Docker Container"
-                sh "docker container create -p 8081:8081 --name LibraryOperations --net libraryconnect -e MYSQL_HOST=mysqldb -e MYSQL_USER=root -e MYSQL_PASSWORD=root -e MYSQL_PORT=3306 libraryoperations"
+                dir("library-operations-loan-example/one-library-operations-example") {
+                    sh "docker container create -p 8081:8081 --name LibraryOperations --net libraryconnect -e MYSQL_HOST=mysqldb -e MYSQL_USER=root -e MYSQL_PASSWORD=root -e MYSQL_PORT=3306 libraryoperations"
+                }
             }
         }
     }
--- library-operations-loan-example/two-library-operations-example/library-second-application-build.groovy
+++ library-operations-loan-example/two-library-operations-example/library-second-application-build.groovy
@@ -6,28 +6,28 @@ pipeline {
     }
 
     stages {
-        stage ('Cloning Repository') {
-            steps {
-                echo "Cloning Repository"
-                sh "git clone https://github.com/DannyPMazy/library-example-rest.git"
-            }
-        }
         stage ("Building Application") {
             steps {
                 echo "Using Maven Install"
-                sh "mvn install"
+                dir("library-operations-loan-example/two-library-operations-example") {
+                    sh "mvn install"
+                }
             }
         }
         stage ("Create Docker Image") {
             steps {
                 echo "Creating a Docker Image"
-                sh "docker build -t libraryloan ."
+                dir ("library-operations-loan-example/two-library-operations-example/") {
+                    sh "docker build -t libraryloan ."
+                }
             }
         }
         stage ("Create Docker Container") {
             steps {
                 echo "Creating a Docker Container"
-                sh "docker container create -p 8082:8082 --name LibraryLoan --net libraryconnect -e MYSQL_HOST=mysqldb -e MYSQL_USER=root -e MYSQL_PASSWORD=root -e MYSQL_PORT=3306 libraryloan"
+                dir ("library-operations-loan-example/two-library-operations-example/") {
+                    sh "docker container create -p 8082:8082 --name LibraryLoan --net libraryconnect -e MYSQL_HOST=mysqldb -e MYSQL_USER=root -e MYSQL_PASSWORD=root -e MYSQL_PORT=3306 libraryloan"
+                }
             }
         }
     }
